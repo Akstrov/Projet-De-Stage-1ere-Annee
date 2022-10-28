368450c9e68102ea9a0c04d836bea47f69b7d36a
Akstrov <yalam385.ya@gmail.com>
2022-10-28 20:16:00 +0100

    Add project files.

---
+++ MinisterePortailWeb/Controllers/E-ServicesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ministere_Portail_Web.Controllers
+{
+    public class E_ServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MinisterePortailWeb/Controllers/MissionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ministere_Portail_Web.Controllers
+{
+    public class MissionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
